423bdf3fcfe53bdfc9fd53202aa406e0dc9bcee8
Christopher Bankes <christopherbankes@yahoo.com>
2019-09-12 13:58:46 -0400

    Log message on app launch made a button

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
     }
 
 
